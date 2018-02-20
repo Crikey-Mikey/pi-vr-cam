508531e66b7aefea57f4fb3ce696853c19f7903f
Mike <mick.lewis84@gmail.com>
2018-02-20 05:16:49 +0000

    add server/websockets

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
-/**
- * Module dependencies.
- */
 const http    = require('http');
 const express = require('express');
 
-// var io = require('socket.io')(http);
+const WebStreamerServer = require('./lib/server.js');
 
 const app  = express();
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
-const server = http.createServer(app);
+const server  = http.createServer(app);
+const silence = new WebStreamerServer(server);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-//   socket.on('disconnect', function(){
-//     console.log('user disconnected');
-//   });
-// });
-
-server.listen(3000, function(){
-  console.log('App is running at http://localhost:3000');
+server.listen(8080, function(){
+  console.log('App is running at http://[IP]:8080');
   console.log('Press CTRL-C to stop\n');
 });

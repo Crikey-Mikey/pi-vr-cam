315fe2cd6e97d8ccc835ca63cc6fe2066e88f7fc
Mike <mick.lewis84@gmail.com>
2018-03-14 05:48:08 +0000

    add servo class

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http    = require('http');
 const express = require('express');
 
-const WebStreamerServer = require('./lib/server.js');
+const WebServer = require('./lib/server.js');
 
 const app  = express();
 
 app.use(express.static(__dirname + '/public'));
 
 const server  = http.createServer(app);
-const silence = new WebStreamerServer(server);
+const silence = new WebServer(server);
 
 server.listen(8080, function(){
   console.log('App is running at http://[IP]:8080');
